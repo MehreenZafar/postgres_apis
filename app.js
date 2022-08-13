b3c2d2b13d395e2eba5fc40cc06eb3110e67b939
MehreenZafar <mtooba.zafar@gmail.com>
2022-08-13 11:16:27 -0700

    env impot changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
+const dotenv= require('dotenv');
 // const port = 3000
 const PORT = process.env.PORT || 3000;
 
